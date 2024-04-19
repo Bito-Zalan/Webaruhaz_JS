524f5588cd1fc145f101e8faf7590b3477c7eb81
Bito-Zalan <bitozalan23@gmail.com>
2024-04-19 13:13:31 +0200

    osszes oldalszerkezet kesz + 90% reszponziv

---
+++ adatok.js
@@ -0,0 +1,17 @@
+export const CIPOK = [
+    {
+        tipus: "Magasszárú",
+        meret: 42,
+        szin: "Kék",
+    },
+    {
+        tipus: "Alacsonyszárú",
+        meret: 44,
+        szin: "Sárga",
+    },
+    {
+        tipus: "Gyerek",
+        meret: 34,
+        szin: "Fekete",
+    },
+];
---
+++ main.js
@@ -0,0 +1,1 @@
+import { CIPOK } from "./adatok.js";
